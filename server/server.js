7c92cf38604c00a2e5d6ec91b419d7c8b46e155e
itsanmol27 <anmolgarg271005@gmail.com>
2025-03-12 13:00:15 +0530

    add ai based doubt solver

--- server/server.js
+++ server/server.js
@@ -3,11 +3,15 @@ import dotenv from 'dotenv';
 import TestRoute from "./routes/test.js"
 import DoubtRoute from "./routes/doubt.js"
 import dbConnect from './utils/dbConnect.js';
+import cors from "cors"
+import fileUpload from 'express-fileupload';
 
 const app = express();
 dotenv.config();
 dbConnect();
 
+app.use(cors());
+app.use(fileUpload());
 app.use(express.json());
 app.use("/test" , TestRoute)
 app.use("/doubt" , DoubtRoute)
